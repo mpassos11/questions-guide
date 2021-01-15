f8329e8a4b95495c750b5c108e9f77ef7a75578c
Matheus Passos <msena789@gmail.com>
2021-01-15 17:54:28 -0300

    Add Boostrap

--- index.js
+++ index.js
@@ -7,44 +7,8 @@ app.set('view engine', 'ejs');
 // Configure static files in EJS
 app.use(express.static('public'));
 
-app.get("/:name/:lang", (req, res) => {
-    var name = req.params.name;
-    var lang = req.params.lang;
-    var showMsg = false;
-    var products = [
-        {
-            name: 'Doritos',
-            price: 3.14
-        },
-        {
-            name: 'Coca-Cola',
-            price: 5
-        },
-        {
-            name: 'Milk',
-            price: 1.45
-        },
-        {
-            name: 'Carne',
-            price: 15
-        },
-        {
-            name: 'RedBull',
-            price: 6
-        },
-        {
-            name: 'Nescau',
-            price: 4
-        }
-    ];
-
-    res.render('index', {
-        nome: name,
-        lang: lang,
-        empresa: "Guia do Programador",
-        msg: showMsg,
-        products: products
-    });
+app.get("/", (req, res) => {
+    res.render('index');
 });
 
 app.listen(4000, () => { console.log('App working!'); });
