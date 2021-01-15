be5252f2ebcdd39beb586f26da68d7c8983424c8
Matheus Passos <msena789@gmail.com>
2021-01-15 17:22:03 -0300

    Displaying dynamic variables in the EJS

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express();
 // configure EJS view engine
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render('index');
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render('index', {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do Programador",
+    });
 });
 
-app.listen(3000, () => { console.log('App rodando!'); });
+app.listen(4000, () => { console.log('App rodando!'); });
