bb51d5f7d8b39e12f05005beb7eabe6d0139716e
Matheus Passos <msena789@gmail.com>
2021-01-15 17:27:28 -0300

    Conditional statements in the EJS

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.set('view engine', 'ejs');
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false;
     res.render('index', {
         nome: nome,
         lang: lang,
         empresa: "Guia do Programador",
+        msg: exibirMsg
     });
 });
 
